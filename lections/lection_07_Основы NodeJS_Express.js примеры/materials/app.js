955dfdf5ad3cb904c7f06a434f678a41356e63dc
savelii.sychov <savelii.sychov@myheritage.com>
2018-08-19 11:46:19 +0300

    L_7 upd

--- lections/lection_07_Основы NodeJS_Express.js примеры/materials/app.js
+++ lections/lection_07_Основы NodeJS_Express.js примеры/materials/app.js
@@ -4,39 +4,71 @@ let port = 3780;
 
 
 
+
+// let data = 'asfnkjafkjakfmaks\n';
+
+// fs.appendFile('log.txt', data, function (err) {
+//     if (err) throw err;
+//     console.log('Saved!');
+// });
+
+
 // MEAN - MongoDb, Express.js, Angular, Node.js 
 console.log(1);
 
+// app.use(function (req, res, next) {
+//   console.log('-----------------------');
+//   console.log('Time:', Date.now());
+//   next();
+// });
+
+let access = {
+    msg: '',
+    state: true
+}
+
 app.use(function (req, res, next) {
-  console.log('-----------------------');
-  console.log('Time:', Date.now());
-  next();
+    if (!access.state) {
+        return next(new Error());
+    }
+    return next();
+});
+
+app.use('/hack', function (req, res, next) {
+    console.log('hack');
+    access.state = false;
+    next(new Error());
 });
 
 app.use(function (req, res, next) {
-    console.log('next');
+    console.log('standart');
     next();
 });
 
-app.get('/foo', function (req, res, next) {
-    console.log('Request Type:', req.method);
+app.use(function (error, req, res, next) {
+    console.log('error');
+    next();
+});
+
+// app.get('/foo', function (req, res, next) {
+//     console.log('Request Type:', req.method);
     
-    // res.send('hello /foo');
-    next();
-});
+//     // res.send('hello /foo');
+//     next();
+// });
 
-app.get('/foo', function (req, res, next) {
-    console.log('second Request Type:', req.method);
+// app.get('/foo', function (req, res, next) {
+//     console.log('second Request Type:', req.method);
 
-    // res.send('foo');
-    next();
-});
+//     // res.send('foo');
+//     next();
+// });
 
-app.use(function (req, res, next) {
-    console.log('next');
+// app.use(function (req, res, next) {
+//     console.log('next');
 
-    next();
-});
+//     next();
+// });
 
 app.listen(port);
 
