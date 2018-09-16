84c67bbd583123131f0852b2aef4730c4ed6784c
SychovSaveliy <savelii.sychov@gettyimages.com>
2018-09-16 11:09:37 +0300

    L_10 :Q 

---
+++ lections/lection_10_vuejs_cli/materials/fr-pro-plus/src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <hr>
+    <!-- {{ list }}
+    <button v-on:click="list = getList()">Click</button>
+    <hr>
+    {{ foobar }}   -->
+    <input type="number" v-model="value">
+    
+    <!-- <some 
+      v-bind:val="value"
+      v-on:input="onChange"></some>   -->
+
+      <some v-model="value">
+        <footer v-if="value == 100" slot="footer">
+          Footer
+        </footer>
+        Hello
+        <ul>
+          <li v-for="item of list" v-bind:key="item">
+            {{ item }}
+          </li>
+        </ul>
+
+
+        <h2 slot="header">Header</h2>
+
+        <br>
+        World!
+      </some> 
+  </div>
+</template>
+
+<script>
+import some from './components/some';
+
+export default {
+  name: 'App',
+  components: {
+    some
+  },
+  data: function(){
+    return {
+      list: [1, 2, 3],
+      value: 10
+    }
+  },
+  methods: {
+    getList() {
+        return this.list.reverse();
+    },
+    onChange(val){
+      this.value = val;
+    }
+  },
+  computed: {
+    foobar() {
+        return this.value + 200;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ lections/lection_10_vuejs_cli/materials/fr-pro-plus/src/components/some.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="some">
+        <h1>Hello {{ name }}</h1>
+        val: {{ val }}
+        inner input: 
+        <!-- <input 
+            type="number" 
+            v-bind:value="val"
+            v-on:input="onChange($event)">   -->
+        <input 
+            type="number" 
+            v-bind:value="val"
+            v-on:input="onChange($event)">
+        
+        <hr>
+
+        <slot></slot>    
+        <slot name="header"></slot>   
+        <slot name="footer"></slot>    
+    </div>
+</template>
+
+
+<script>
+export default {
+    model: {
+        prop: 'val',
+        event: 'input'
+    },
+    props: ['val'],
+    data: function(){
+        return {
+            name: 'Vasya'
+        }
+    },
+    methods: {
+        onChange(ev){
+            this.$emit('input', ev.target.value);
+        }
+    }
+}
+</script>
+
+
+<style>
+    .some {
+        color: red;
+    }
+</style>
